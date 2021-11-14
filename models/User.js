e6f0b82d5f6c543ab665e44c39409c6e8d86b8fc
cshepscorp <sheppard.christy@gmail.com>
2021-11-14 17:24:06 -0500

    passwords re hashing

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@
 /* This Model class is what we create our own models from using the extends keyword so User inherits all of the functionality the Model class has.*/
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -49,18 +50,34 @@ User.init(
     }
   },
   {
+    // The nested level of the object inserted is very important. Notice that the hooks property was added to the second object in User.init()
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      // The keyword pair, async/await, works in tandem to make this async function look more like a regular synchronous function expression
+      async beforeCreate(newUserData) { // The async keyword is used as a prefix to the function that contains the asynchronous function
+        // await can be used to prefix the async function, which will then gracefully assign the value from the response to the newUserData's password property
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData; // newUserData is then returned to the application with the hashed password
+      },
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+  }
+    },
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
     // configures certain options for the table
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
+        // don't automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
-    // don't pluralize name of database table
+        // don't pluralize name of database table
     freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+        // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
     underscored: true,
-    // make it so our model name stays lowercase in the database
+        // make it so our model name stays lowercase in the database
     modelName: 'user'
+    
   }
 );
 
